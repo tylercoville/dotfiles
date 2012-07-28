c9e4861cbc597ff72e1c59b57eb2bd045a840072
Tyler Coville <tjcoville@gmail.com>
2012-07-27 18:11:37 -0700

    Added powerline styling to tmux and other misc changes.

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
 
-export PATH="/usr/local/lib/node_modules:/usr/local/share/python:./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export PATH="/usr/local/lib/node_modules:/usr/local/share/python:./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/usr/gcc-4.7.1/bin:$PATH"
+export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:/usr/gcc-4.7.1/share/man/:$MANPATH"
