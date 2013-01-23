d7820711601e07757c95e4f5d85f30e2b87ffc01
Tyler Coville <tcoville@playfirst.com>
2013-01-23 12:55:15 -0800

    Updated path for NPM

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
 
-export PATH="/usr/local/lib/node_modules:/usr/local/share/python:./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="/usr/local/share/npm/bin:/usr/local/lib/node_modules:/usr/local/share/python:./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
