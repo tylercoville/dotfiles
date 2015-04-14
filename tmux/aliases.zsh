b91a68329b1a4e8663236c6fee90926e7dd38bd1
Tyler Coville <tcoville@playfirst.com>
2015-04-14 11:23:08 -0700

    Working on tmux commands

--- tmux/aliases.zsh
+++ tmux/aliases.zsh
@@ -1,7 +1,10 @@
-alias tmux="TERM=screen-256color-bce tmux"
+alias tmux="TERM=screen-256color-bce tmux -u"
 alias tma='tmux attach -d -t'
-alias tmux-new='tmux new -s $(basename $(pwd))'
-
-alias tma='tmux attach -d -t'
-alias git-tmux='tmux new -s $(basename $(pwd))'
-
+function tmux-new() {
+  if [ "$1" != "" ] 
+  then
+    tmux new -s $1
+  else
+    tmux new -s $(basename $(pwd))
+  fi
+}
